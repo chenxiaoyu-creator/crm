ad5fffac99252ffd57fb08d6452fc2ce282f3e4a
chenxiaoyu-creator <3283146539@qq.com>
2020-12-01 11:59:17 +0800

    第二次更改

--- 001-git/src/main/java/Test.java
+++ 001-git/src/main/java/Test.java
@@ -1,6 +1,9 @@
 public class Test {
     public static void main(String[] args) {
         int i=0;
+        int a=15;
         System.out.println (122);
+        System.out.println (15);
+
     }
 }
